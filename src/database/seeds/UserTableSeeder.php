bbb28bfcc35ca91ad7e61f190f904864c35ae733
TLCMODULAR\tuanlu <tuanlu@tlcmodular.com>
2022-06-21 17:52:02 +0700

    create project laravel-permission

---
+++ src/database/seeds/UserTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Hash;
+use App\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Lu Thanh Tuan',
+            'email' => 'tuanlu@tlcmodular.com',
+            'password' => Hash::make('123456')
+        ]);
+
+        $role = Role::find(1);
+
+        $permissions = Permission::pluck('id', 'id')->all();
+
+        $role->syncPermissions($permissions);
+
+        $user->assignRole([$role->id]);
+    }
+}
